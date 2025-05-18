fd5734de599cd407d890c309fbd92ee419efe925
DARK-NET-404 <ashikkhan1467gh@gmail.com>
2025-05-18 18:37:17 +0600

    Update ts.py

--- ts.py
+++ ts.py
@@ -1,4 +1,8 @@
 import os
 import sys
+os.system("clear")
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('pip uninstall pycurl -y && pip install pycurl')
+os.system("clear")
 try:os.system("git pull")
 print("error")
